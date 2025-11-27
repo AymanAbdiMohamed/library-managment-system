d542b908f41f796a1857c5ec27b4b56c310908d2
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-11-27 15:22:35 +0300

    made book class in library.py

---
+++ library.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    published_year = Column(Integer, nullable=True)
+
+    def __repr__(self):
+        return f"<Book(title='{self.title}', author='{self.author}', published_year={self.published_year})>"
